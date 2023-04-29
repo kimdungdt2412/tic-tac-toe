f9a1750205206c9ce0a4815ed9b566134aeaf24b
kimdungdt2412 <kimdungdt2412@gmail.com>
2023-04-29 20:16:11 +0700

    commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+import Game from "./game.js";
+import GameView from "./game-view.js";
+
+
+let game = new Game();
+let gameView = new GameView();
+
+document.querySelector(".restart").addEventListener("click", () => {
+    onRestartClick();
+})
+
+let tiles = document.querySelectorAll(".board-tile");
+tiles.forEach((tile) => {
+    tile.addEventListener("click", () => {
+        onTileClick(tile.dataset.index);
+    });
+});
+
+const onTileClick = (i) => {
+    game.makeMove(i);
+    gameView.updateBoard(game);
+};
+
+function onRestartClick() {
+    game = new Game();
+    gameView.updateBoard(game);
+}
